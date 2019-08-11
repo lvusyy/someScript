34941d8ec694c5ec0bec21561ac9fe821f4731a1
lvusyy <mlive2083@gmail.com>
2019-08-11 22:54:07 +0800

    Update fedora换腾讯源.sh

--- fedora换腾讯源.sh
+++ fedora换腾讯源.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 mv /etc/yum.repos.d/fedora.repo /etc/yum.repos.d/fedora.repo.backup
 mv /etc/yum.repos.d/fedora-updates.repo /etc/yum.repos.d/fedora-updates.repo.backup
+#rm /etc/yum.repos.d/*.repo
 curl -o /etc/yum.repos.d/fedora.repo http://mirrors.cloud.tencent.com/repo/fedora.repo
 curl -o /etc/yum.repos.d/fedora-updates.repo http://mirrors.cloud.tencent.com/repo/fedora-updates.repo
+
 dnf makecache
+
+#清华大学的fedora源
+#https://mirrors.tuna.tsinghua.edu.cn/help/fedora/ 
