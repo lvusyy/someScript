775453ee8da722c65868844da83b8fbb8e742495
lvusyy <mlive2083@gmail.com>
2019-08-12 00:53:31 +0800

    Update docker-ce安装.sh

--- docker-ce安装.sh
+++ docker-ce安装.sh
@@ -7,8 +7,11 @@
 yum makecache  
 yum install docker-ce -y
 
-
-
+##修改为国内仓库源
+mkdir -p /etc/docker
+cat << FFF >> /etc/docker/daemon.json
+{"registry-mirrors":["https://docker.mirrors.ustc.edu.cn","https://hub-mirror.c.163.com","https://registry.docker-cn.com"]}
+FFF
 
 
 #其他系统
